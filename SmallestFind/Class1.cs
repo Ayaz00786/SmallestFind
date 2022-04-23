1d7e3b1d05332dc90667b78402af2ae44701f332
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-23 23:17:09 +0530

    Add project files.

---
+++ SmallestFind/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmallestFind
+{
+    internal class Class1
+    {
+        public static void findsmall()
+        {
+            Console.WriteLine("enter 1st number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter 1st number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter 1st number:");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 < num2 && num1 < num3)
+            {
+                Console.WriteLine("Smallest Number Is: {0}", num1);
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                Console.WriteLine("Smallest Number Is: {0}", num2);
+            }
+            else
+            {
+                Console.WriteLine("Smallest Number Is: {0}", num3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SmallestFind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallestFind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.findsmall();
+        }
+    }
+}
